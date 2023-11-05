b9dd8dbcd9c46e16ed7b79da5be22f4b3bbbaee3
raunak <rksj2001@gmail.com>
2023-11-05 23:02:54 +0530

    added form

--- lib/gist_web/live/create_gisti_live.ex
+++ lib/gist_web/live/create_gisti_live.ex
@@ -1,7 +1,14 @@
 defmodule GistWeb.CreateGistiLive do
   use GistWeb, :live_view
+  import Phoenix.HTML.Form
+  alias Gist.{Gistis, Gistis.Gisti}
 
   def mount(_params, _session, socket) do
+    socket = assign(
+      socket,
+      form: to_form(Gistis.change_gisti(%Gisti{}))
+    )
+
     {:ok, socket}
   end
 end
